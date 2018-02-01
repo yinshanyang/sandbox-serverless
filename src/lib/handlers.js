281f33322c2beaf650a867088d9acde2912bf8f2
Yin Shanyang <me@yinshanyang.com>
2018-02-02 02:10:42 +0800

    feature: offline dynamodb

--- src/lib/handlers.js
+++
@@ -1,6 +0,0 @@
-// @flow
-export const hello = (event: Object, context: Object, callback: Function): void =>
-  callback(null, 'hello')
-
-export const bye = (event: Object, context: Object, callback: Function): void =>
-  callback(null, 'bye')
